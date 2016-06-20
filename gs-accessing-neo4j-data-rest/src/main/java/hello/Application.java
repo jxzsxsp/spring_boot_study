f2dfd8d7f16a4c4cd6fef05e3323e656d7c40237
Sean <sean.xiao@lianshang.cn>
2016-06-20 19:19:14 +0800

    添加Accessing Neo4j Data with REST Demo

---
+++ gs-accessing-neo4j-data-rest/src/main/java/hello/Application.java
@@ -0,0 +1,29 @@
+package hello;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.config.Neo4jConfiguration;
+
+@SpringBootApplication
+@EnableNeo4jRepositories
+public class Application extends Neo4jConfiguration {
+
+	public Application() {
+		setBasePackage("hello");
+	}
+
+	@Bean(destroyMethod = "shutdown")
+	public GraphDatabaseService graphDatabaseService() {
+		return new GraphDatabaseFactory().newEmbeddedDatabase("target/hello.db");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
