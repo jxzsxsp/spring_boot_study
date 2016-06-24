1c326af83b3f80650e5fee2564da66ea134feefb
Sean <sean.xiao@lianshang.cn>
2016-06-24 12:13:42 +0800

    添加React.js and Spring Data REST - Security Demo

---
+++ react-and-spring-data-rest-security/src/main/resources/static/webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+var node_dir = __dirname + '/node_modules';
+
+module.exports = {
+    entry: './app.js',
+    devtool: 'sourcemaps',
+    cache: true,
+    debug: true,
+    resolve: {
+        alias: {
+            'stompjs': node_dir + '/stompjs/lib/stomp.js',
+        }
+    },
+    output: {
+        path: __dirname,
+        filename: './built/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
