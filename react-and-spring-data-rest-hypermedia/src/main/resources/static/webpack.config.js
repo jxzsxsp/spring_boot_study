0b7990680fc0668e31d7f25caf7877b0013c3011
Sean <sean.xiao@lianshang.cn>
2016-06-24 13:43:51 +0800

    添加React.js and Spring Data REST - Hypermedia Demo

---
+++ react-and-spring-data-rest-hypermedia/src/main/resources/static/webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+var node_dir = __dirname + '/node_modules';
+
+module.exports = {
+    entry: './app.js',
+    devtool: 'sourcemaps',
+    cache: true,
+    debug: true,
+    output: {
+        path: __dirname,
+        filename: './built/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
