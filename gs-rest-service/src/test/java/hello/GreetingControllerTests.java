d49749f2be2ca0633d82d7c130fdd0a8690afd93
Sean <sean.xiao@lianshang.com>
2018-01-15 20:22:09 +0800

    fix

--- gs-rest-service/src/test/java/hello/GreetingControllerTests.java
+++ gs-rest-service/src/test/java/hello/GreetingControllerTests.java
@@ -25,7 +25,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -36,7 +35,6 @@ import org.springframework.web.context.WebApplicationContext;
  * @author Greg Turnquist
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class GreetingControllerTests {
 
